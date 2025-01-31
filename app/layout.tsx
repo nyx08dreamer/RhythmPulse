e083a9f1884a16b9be6cd53129ec578c76605a8d
Stefano Parra <stefanoparra15@gmail.com>
2025-01-30 23:03:17 -0400

    Primer avance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -16,10 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={font.className}
-      >
-        {children}
+      <body className={font.className}>
+        <Sidebar>
+          {children}
+        </Sidebar>
       </body>
     </html>
   );
